d5543295d23b96bc652755c16f3857df9c13f0b5
RasmusUCL <47382081+RasmusUCL@users.noreply.github.com>
2019-05-23 12:51:29 +0200

    Lunde 0,4
    Billetside 2 tilføjet

---
+++ Lunde-0,4/Java/Nav-toggle.js
@@ -0,0 +1,8 @@
+function navToggle() {
+  var x = document.getElementById("navigation-bar");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
